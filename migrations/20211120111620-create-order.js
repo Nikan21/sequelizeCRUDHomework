71e9f7ed5e2c47ec674134c30dbaf33b3a006241
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-11-20 13:28:06 +0200

    Update migration

--- migrations/20211120111620-create-order.js
+++ migrations/20211120111620-create-order.js
@@ -1,39 +1,56 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Orders', {
+    await queryInterface.createTable("orders", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       nameOrders: {
-        type: Sequelize.STRING
+        field: "name_orders",
+        type: Sequelize.STRING(256),
+        allowNull: false,
       },
       idUser: {
-        type: Sequelize.INTEGER
+        field: "id_user",
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "User",
+          key: "id",
+        },
       },
       quantity: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
       },
       isSend: {
-        type: Sequelize.BOOLEAN
+        field: "is_send",
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
       },
       dateSend: {
-        type: Sequelize.DATE
+        field: "date_send",
+        type: Sequelize.DATE,
+        allowNull: false,
       },
       createdAt: {
+        field: "created_at",
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
+        field: "updateAt",
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Orders');
-  }
+    await queryInterface.dropTable("orders");
+  },
 };
