91673d29be243f0fb9140426a26adad9df130e01
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-11-20 12:22:28 +0200

    Fix name

---
+++ models/users.js
@@ -0,0 +1,20 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    static associate(models) {
+    }
+  };
+  Users.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    birthday: DataTypes.DATEONLY
+  }, {
+    sequelize,
+    modelName: 'Users',
+  });
+  return Users;
+};
