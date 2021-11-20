019f016aa855c932b922ed711a530089c0ba1f1f
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-11-20 12:40:59 +0200

    Add restrictions

--- models/users.js
+++ models/users.js
@@ -1,22 +1,30 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
-    static associate(models) {
+    static associate(models) {}
+  }
+  Users.init(
+    {
+      firstName: {
+        field: "first_name",
+        type: DataTypes.STRING(64),
+        allowNull: false,
+      },
+      lastName: {
+        field: "last_name",
+        type: DataTypes.STRING(128),
+        allowNull: false,
+      },
+      email: { type: DataTypes.STRING(256), allowNull: false },
+      birthday: { type: DataTypes.DATEONLY, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "Users",
+      tableName: "users",
+      underscore: true,
     }
-  };
-  Users.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    birthday: DataTypes.DATEONLY
-  }, {
-    sequelize,
-    modelName: 'Users',
-    tableName: 'users',
-    underscore: true
-  });
+  );
   return Users;
 };
