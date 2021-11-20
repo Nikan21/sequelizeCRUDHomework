d863057769449019a8b461dd9fe6ad030334a351
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-11-20 13:01:08 +0200

    Add validation

--- models/user.js
+++ models/user.js
@@ -9,15 +9,43 @@ module.exports = (sequelize, DataTypes) => {
       firstName: {
         field: "first_name",
         type: DataTypes.STRING(64),
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+          notNull: true,
+          notEmpty: true,
+          len: [2, 64]
+        },
       },
       lastName: {
         field: "last_name",
         type: DataTypes.STRING(128),
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+          notNull: true,
+          notEmpty: true,
+          len: [2, 128]
+        },
       },
-      email: { type: DataTypes.STRING(256), allowNull: false },
-      birthday: { type: DataTypes.DATEONLY, allowNull: false },
+      email: {
+        type: DataTypes.STRING(256),
+        allowNull: false,
+        validate: {
+          isEmail: true,
+          notNull: true,
+          notEmpty: true
+        },
+      },
+      birthday: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        validate: {
+          isDate: true,
+          notNull: true,
+          notEmpty: true
+        },
+      },
     },
     {
       sequelize,
