7af8e46e6c0116ce23bca606f63bab263b8d7c13
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-11-21 14:47:42 +0200

    Update function in order router file

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,13 +1,14 @@
 const ordersRouter = require("express").Router();
+const orderController = require ("../controllers/orders.controller")
 
 ordersRouter
 .route('/')
-.get()
-.post()
+.get(orderController.getAllOrders)
+.post(orderController.createOrders)
 
 .route('/:id')
-.get()
-.update()
-.delete()
+.get(orderController.getOrderByID)
+.update(orderController.updateOrder)
+.delete(orderController.deleteOrder)
 
 module.exports = ordersRouter;
